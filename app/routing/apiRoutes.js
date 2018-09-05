7dd77439e48e16652db36e3534218a1b013a2aef
Vance <jasonkirker@users.noreply.github.com>
2018-09-04 23:01:47 -0500

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,24 +8,6 @@ router.get("/api/friends", function(req, res) {
     
 
     console.log(friends);
-  
-    // for (var i = 0; i < friends.length; i++) {
-        return res.json(friends);
-    }
-    // return res.json(false);
-  );
-
-router.post("/api/friends", function(req, res){
-    var score = [];
-    for(i = 0; i < 10; i++){
-    score.push(parseFloat(req.body.scores[i]))
-    }
-    for(k = 0; k < friends.length; k++){
-    for(i = 0; i < 10; i++){
-        var totalArray = [];
-        Math.abs(score[i] - friends[k].scores[i])
-    }
-    }
-});
+ 
 
 module.exports = router;
